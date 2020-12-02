84d2f4c29e07aad8d2808db56746bf8b10fc6102
SonaliSinha <sonalisinha22@gmail.com>
2020-12-02 22:03:26 +0530

    Platform Added

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-//platform added
+
     platform = new Ground(150,305,300,170);
 
     box1 = new Box(700,320,70,70);
@@ -64,4 +64,5 @@ function draw(){
     log5.display();
 
     bird.display();
-}
+
+
